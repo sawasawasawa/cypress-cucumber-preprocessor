cec368c36f5ce01801f514bf0ab12a01de10175d
Flavio Curella <flavio.curella@gmail.com>
2018-01-22 17:22:43 -0600

    implement as transform

---
+++ index.js
@@ -0,0 +1,113 @@
+const fs = require('fs');
+const EventEmitter = require('events').EventEmitter;
+const through = require('through');
+
+const browserify = require('@cypress/browserify-preprocessor');
+
+const log = require('debug')('cypress:cucumber');
+const glob = require('glob');
+
+const watchers = {};
+
+// This is the template for the file that we will send back to cypress instead of the text of a
+// feature file
+const createCucumber = (spec, definitions) => (
+  `
+  const {resolveAndRunStepDefinition, given, when, then} = require('cypress-cucumber-preprocessor/resolveStepDefinition');
+  ${eval(definitions).join('\n')}
+  const {Parser, Compiler} = require('gherkin');
+  const spec = \`${spec}\`
+  const gherkinAst = new Parser().parse(spec);
+  describe(gherkinAst.feature.name, () => {
+    gherkinAst.feature.children.forEach(createTestFromScenario);
+  });
+  function createTestFromScenario (scenario) {
+    describe(scenario.name, () => {
+        scenario.steps.map(
+          step => it(step.text, () => {
+            resolveAndRunStepDefinition(step)
+          })
+        )
+      }
+    )
+  }`
+);
+
+const pattern = `${process.cwd()}/cypress/support/step_definitions/**.js`;
+const stepDefinitionsPaths = [].concat(glob.sync(pattern));
+
+const compile = (spec) => {
+  log('compiling', spec);
+
+  const definitions = [];
+  stepDefinitionsPaths.forEach((path) => {
+    definitions.push(
+      `{ ${fs.readFileSync(path).toString().replace('cypress-cucumber-preprocessor', 'cypress-cucumber-preprocessor/resolveStepDefinition')}
+      }`
+    );
+  });
+
+  return createCucumber(spec, JSON.stringify(definitions));
+};
+
+
+const touch = (filename) => {
+  fs.utimesSync(filename, new Date(), new Date());
+};
+
+
+const transform = (file) => {
+  let data = '';
+
+  function write(buf) { data += buf; }
+  function end() {
+    if (file.match('.feature$')) {
+      log('compiling feature ', file);
+      this.queue(compile(data));
+    } else {
+      this.queue(data);
+    }
+    this.queue(null);
+  }
+
+  return through(write, end);
+};
+
+
+const preprocessor = (options) => {
+  return file => {
+
+    options.browserifyOptions.transform.unshift(
+      transform
+    );
+
+    if (file.shouldWatch) {
+
+      stepDefinitionsPaths.forEach((stepPath) => {
+        if (watchers[stepPath] === undefined) {
+          const stepFile = new EventEmitter();
+          stepFile.filePath = stepPath;
+
+          const bundleDir = file.outputPath.split('/').slice(0, -2);
+          const outputName = stepPath.split('/').slice(-3);
+          stepFile.outputPath = bundleDir.concat(outputName).join('/');
+          stepFile.shouldWatch = file.shouldWatch;
+
+          stepFile.on('rerun', () => {
+            touch(file.filePath);
+          });
+          watchers[stepPath] = browserify(options)(stepFile);
+        } else {
+          log(`Watcher already set for ${stepPath}`);
+        }
+      })
+
+    }
+    return browserify(options)(file);
+  };
+};
+
+module.exports = {
+  default: preprocessor,
+  transform: transform,
+};
---
+++ resolveStepDefinition.js
@@ -0,0 +1,70 @@
+const path = require('path');
+
+const {CucumberExpression, ParameterTypeRegistry} = require('cucumber-expressions');
+
+const parameterTypeRegistry = new ParameterTypeRegistry();
+
+class StepDefinitionRegistry {
+  constructor() {
+    this.definitions = {};
+    this.runtime = {};
+    this.latestType = '';
+
+    ['given', 'when', 'then'].forEach(keyword => {
+      this.definitions[keyword] = [];
+      this.runtime[keyword] = (expression, implementation) => {
+        this.definitions[keyword].push({
+
+          implementation,
+          expression: new CucumberExpression(expression, parameterTypeRegistry)
+        })
+      };
+    })
+
+    this.load = definitionFactoryFunction => definitionFactoryFunction(this.runtime);
+
+    this.resolve = (type, text) => {
+      if (type === 'and') {
+        type = this.latestType;
+      }
+
+      if (this.definitions[type]) {
+        this.latestType = type;
+        return this.definitions[type].filter(({expression}) => expression.match(text))[0]
+      }
+    }
+  }
+
+}
+
+const stepDefinitionRegistry = new StepDefinitionRegistry();
+
+function resolveStepDefinition(step) {
+  const stepDefinition = stepDefinitionRegistry.resolve(step.keyword.toLowerCase().trim(), step.text);
+
+  return stepDefinition || {};
+}
+
+module.exports = {
+  resolveAndRunStepDefinition: (step) => {
+
+    const {expression, implementation} = resolveStepDefinition(step)
+    if (expression && implementation) {
+      return implementation(
+        ...expression.match(step.text).map(match => match.getValue())
+      )
+    } else {
+      throw new Error(`Step implementation missing for: ${step.text}`)
+    }
+  },
+  given: (expression, implementation) => {
+    stepDefinitionRegistry.runtime.given(expression, implementation)
+  },
+  when: (expression, implementation) => {
+    stepDefinitionRegistry.runtime.when(expression, implementation)
+
+  },
+  then: (expression, implementation) => {
+    stepDefinitionRegistry.runtime.then(expression, implementation)
+  }
+}
